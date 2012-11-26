60d49db86cdc1c521fd3a8b264e0aa35c9b95541
Ryan W Tenney <ryan@10e.us>
2012-11-26 00:21:43 -0500

    Default altitude to NaN, only serialize if not NaN.

--- src/main/java/com/ryantenney/passkit4j/model/Location.java
+++ src/main/java/com/ryantenney/passkit4j/model/Location.java
@@ -1,6 +1,11 @@
 package com.ryantenney.passkit4j.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.Accessors;
@@ -8,12 +13,13 @@ import lombok.experimental.Accessors;
 @Data
 @Accessors(chain=true, fluent=true)
 @RequiredArgsConstructor
+@NoArgsConstructor(access=AccessLevel.PROTECTED)
 public class Location {
 
 	@NonNull private double latitude;
 	@NonNull private double longitude;
 
-	private double altitude = 0.0;
+	@JsonInclude(Include.NON_DEFAULT) private double altitude = Double.NaN;
 	private String relevantText;
 
 }
